500e2d7310c3b12021c99dc5df7097241ad6bcef
John <jpbadillo05@gmail.com>
2026-08-27 14:04:44 -0400

    Ejercicios de direcciones y pointers basicos

--- direcciones.cpp
+++ direcciones.cpp
@@ -11,12 +11,29 @@ int main() {
     int *ptr = nullptr;
     ptr = &num;
 
-    cout << "ptr apunta a: " << ptr << endl;.
+    cout << "ptr apunta a: " << ptr << endl;
     cout << "Valor apuntado (*ptr): " << *ptr << endl;
 
     // Modificar el valor original a traves del pointer
     *ptr = 100;
     cout << "num despues de *ptr = 100: " << num << endl;
+
+    double precio = 19.99;
+    double *ptrPrecio = &precio;
+
+   // int *ptrNum = &precio;
+
+    cout << "ptrPrecio apunta a: " << ptrPrecio << endl;
+
+    cout << "Valor apuntado (*ptrPrecio): " << *ptrPrecio << endl;
+
+   // cout << "ptrNum apunta a: " << *ptrNum << endl;
+
+    cout << "Me muestra que no se puede convertir un double a int" << endl;
+
+    cout << "Me mostro un numero 2, una respuesta aleatoria" << endl;
+
+    
 
     return 0;
 }
