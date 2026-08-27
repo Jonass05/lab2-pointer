897e2e589edcba69e7af6889fae068f0864e1aad
John <jpbadillo05@gmail.com>
2026-08-27 13:33:52 -0400

    direcciones y pointer basicos

--- direcciones.cpp
+++ direcciones.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main() {
-    cout << "Lab 2 - Pointers" << endl;
-    cout << "Name: John " << endl;
+    int num = 25;
+
+    // Dirección de memoria
+    cout << "Direccion de num: " << &num << endl;
+
+    // Declarar y asignar un pointer
+    int *ptr = nullptr;
+    ptr = &num;
+
+    cout << "ptr apunta a: " << ptr << endl;.
+    cout << "Valor apuntado (*ptr): " << *ptr << endl;
+
+    // Modificar el valor original a traves del pointer
+    *ptr = 100;
+    cout << "num despues de *ptr = 100: " << num << endl;
+
     return 0;
 }
